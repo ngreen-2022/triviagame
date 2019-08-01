db2b65ece914f5871c82348f116d309c932a248e
KathrynLester <lesterkn@uab.edu>
2019-08-01 11:46:37 -0500

    Add link to edit profile

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,67 @@
+import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  getCurrentProfile,
+  deleteAccount,
+  createProfile
+} from '../../actions/profile';
+import Spinner from '../layout/Spinner';
+
+const Profile = ({
+  getCurrentProfile,
+  deleteAccount,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Your Profile</h1>
+      <p className='lead'>
+        <i className='fas fa-user'> Welcome {user && user.name}</i>
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <div className='my-2'>
+            <button className='btn btn-danger' onClick={() => deleteAccount()}>
+              <i className='fas fa-user-minus' /> Delete My Account
+            </button>
+          </div>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <div>
+            <p>You have not yet created a profile, please add some info!</p>
+            <Link className='btn btn-danger' to='/editme'>
+              <i className='fas fa-user-minus' /> Edit Profile
+            </Link>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profile.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+  deleteAccount: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile, deleteAccount }
+)(Profile);
